b8734b0d5b4f3c7b52d6917f90dad5f9714bd785
songyongmeng <songyongmeng@neworangegroup.com>
2023-07-19 07:49:13 +0800

    添加总结页

--- app/src/main/java/com/songyongmeng/gp/SumUpListAdapter.java
+++ app/src/main/java/com/songyongmeng/gp/SumUpListAdapter.java
@@ -31,6 +31,20 @@ public class SumUpListAdapter extends RecyclerView.Adapter<SumUpListAdapter.Vert
     public void onBindViewHolder(VerticalScrollViewHolder holder, int position) {
         holder.sumNameText.setText(dataList.get(position).getGpName());
         holder.sumResultPointText.setText(dataList.get(position).getResultPoint()+"");
+        holder.self_turnover_text.setText(dataList.get(position).getEndQuantity()<5?"缩量":"平放量");
+        holder.sell_open_text.setText(dataList.get(position).getSellOpen()+"");
+        String allTurnover = "";
+        if(dataList.get(position).getAllTurnover()==-1){
+            allTurnover = "缩量";
+        }
+        if(dataList.get(position).getAllTurnover()==0){
+            allTurnover = "平量";
+        }
+        if(dataList.get(position).getAllTurnover()==1){
+            allTurnover = "放量";
+        }
+        holder.all_turnover_text.setText(allTurnover);
+        holder.is_ban_text.setText(dataList.get(position).isIsBuyBan()?"是":"否");
     }
 
 
@@ -42,12 +56,19 @@ public class SumUpListAdapter extends RecyclerView.Adapter<SumUpListAdapter.Vert
     protected static class VerticalScrollViewHolder extends RecyclerView.ViewHolder {
         TextView sumNameText;
         TextView sumResultPointText;
+        TextView self_turnover_text;
+        TextView sell_open_text;
+        TextView all_turnover_text;
+        TextView is_ban_text;
 
         public VerticalScrollViewHolder(View itemView) {
             super(itemView);
             sumNameText = itemView.findViewById(R.id.sum_name_text);
             sumResultPointText = itemView.findViewById(R.id.result_point_text);
-
+            self_turnover_text = itemView.findViewById(R.id.self_turnover_text);
+            sell_open_text = itemView.findViewById(R.id.sell_open_text);
+            all_turnover_text = itemView.findViewById(R.id.all_turnover_text);
+            is_ban_text = itemView.findViewById(R.id.is_ban_text);
         }
     }
 }
